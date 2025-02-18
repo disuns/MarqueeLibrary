f679d86f96a42bc0ccc5f8c0bf6424982940843d
sungjun choi <97460483+disuns@users.noreply.github.com>
2025-02-18 15:49:54 +0900

    [Fix]version (#5)
    * [Feat]library1 Complete Version
    
    * feat/library Setting Set
    
    * feat/1차본(기능 완성)
    
    * feat/1차본 기능추가
    
    * fix/version (#4)
    
    * feat/library Setting Set
    
    * feat/1차본(기능 완성)
    
    * feat/1차본 기능추가
    
    * feat/버전 수정

--- MarqueeLibrary/build.gradle.kts
+++ MarqueeLibrary/build.gradle.kts
@@ -67,7 +67,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.disun"
                 artifactId = "MarqueeLibrary"
-                version = "1.0.0"
+                version = "1.0.3"
             }
         }
     }
